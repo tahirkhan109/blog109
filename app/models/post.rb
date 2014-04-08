20a97c3456aed9b72e279fac7cdb6daa6035c015
tahirlodhi109 <tahir_lodhi109@yahoo.com>
2014-04-08 12:41:36 +0500

    model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments, dependent: :destroy
+  #has_many :comments, dependent: :destroy
   validates_presence_of :text
   validates :title, presence: true,
             length: { minimum: 5 }
