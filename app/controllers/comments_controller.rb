fcea782af0d313827fc441a0ec07bdf960977df3
tahirlodhi109 <tahir_lodhi109@yahoo.com>
2014-04-08 15:23:03 +0500

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+def create
+puts "11111111111111111111111",params.inspect
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.new(params[:comment].permit(:commenter, :body))
+  if @comment.save
+    redirect_to post_path(@post)
+  else
+render 'new'
+  end
+
+end
+
 end
